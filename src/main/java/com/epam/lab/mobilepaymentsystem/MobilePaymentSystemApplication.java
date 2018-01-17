8fd582d27212c8e5e5ef3c25b540d571488f3e8a
vlavik007 <vlavik007@gmail.com>
2018-01-17 23:06:47 +0300

    Create project structure

---
+++ src/main/java/com/epam/lab/mobilepaymentsystem/MobilePaymentSystemApplication.java
@@ -0,0 +1,12 @@
+package com.epam.lab.mobilepaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePaymentSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/epam/lab/mobilepaymentsystem/MobilePaymentSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epam.lab.mobilepaymentsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobilePaymentSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
