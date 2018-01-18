59c0deac8cbad88e9bf6062679eb1d453d399fdb
vlavik007 <vlavik007@gmail.com>
2018-01-18 11:26:51 +0300

    Done minor refactoring

--- src/main/java/com/epam/lab/mobilepaymentsystem/controller/HomeController.java
+++ src/main/java/com/epam/lab/mobilepaymentsystem/controller/HomeController.java
@@ -3,16 +3,15 @@ package com.epam.lab.mobilepaymentsystem.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @Value("${spring.application.name}")
-    String appName;
+    private String appName;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String showHomePage(Model model) {
         model.addAttribute("appName", appName);
         return "index";
