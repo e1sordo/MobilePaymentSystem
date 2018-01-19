92e3e1eea6e87b02e34c140fea94b1a82830f90c
Meissshu <allayarov.marsel4305@mail.ru>
2018-01-19 12:34:10 +0300

    Basic front page and controller for the task
    Issues: cannot pass whole entity as ModelAttribute. Will be fixed soon

--- src/main/java/com/epam/lab/mobilepaymentsystem/controller/BillController.java
+++ src/main/java/com/epam/lab/mobilepaymentsystem/controller/BillController.java
@@ -1,4 +1,18 @@
 package com.epam.lab.mobilepaymentsystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BillController {
+
+    @RequestMapping("/newbill")
+    public String billForm() {
+        return "bill";
+    }
+
+//    @PostMapping("/newbill")
+//    public String billSubmit(@ModelAttribute Bill bill) {
+//        return "index";
+//    }
 }
