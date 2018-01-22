7e0afc3de1f89362fbb11b232feba1f1107fc2e9
asgalantsev <asgalantsev@mail.ru>
2018-01-22 12:10:32 +0300

    Added internalization, not fixed bugs

--- src/main/java/com/epam/lab/mobilepaymentsystem/MobilePaymentSystemApplication.java
+++ src/main/java/com/epam/lab/mobilepaymentsystem/MobilePaymentSystemApplication.java
@@ -2,11 +2,22 @@ package com.epam.lab.mobilepaymentsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
-public class MobilePaymentSystemApplication {
+public class MobilePaymentSystemApplication  extends WebMvcConfigurerAdapter{
 
-	public static void main(String[] args) {
-		SpringApplication.run(MobilePaymentSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MobilePaymentSystemApplication.class, args);
+    }
 }
