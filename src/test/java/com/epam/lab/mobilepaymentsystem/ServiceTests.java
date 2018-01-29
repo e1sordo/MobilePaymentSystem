a4aa6b07b0c95ca924e96539b8c578b8d738777d
Meissshu <allayarov.marsel4305@mail.ru>
2018-01-29 22:54:55 +0300

    Add some tests

---
+++ src/test/java/com/epam/lab/mobilepaymentsystem/ServiceTests.java
@@ -0,0 +1,59 @@
+package com.epam.lab.mobilepaymentsystem;
+
+
+import com.epam.lab.mobilepaymentsystem.model.ServiceUnit;
+import com.epam.lab.mobilepaymentsystem.service.ServiceUnitService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MobilePaymentSystemApplication.class})
+public class ServiceTests {
+
+    @Autowired
+    private ServiceUnitService serviceUnitService;
+
+    private static int count = 0;
+
+    private ServiceUnit createTestService() {
+        ++count;
+        ServiceUnit serviceUnit = new ServiceUnit();
+        serviceUnit.setName("test service" + count);
+        serviceUnit.setCost(100);
+        serviceUnitService.save(serviceUnit);
+        return serviceUnit;
+    }
+
+    @Test
+    public void addService() {
+        ServiceUnit serviceUnit = createTestService();
+        String serviceName = serviceUnit.getName();
+        assertEquals(serviceName,
+                serviceUnitService.getByServiceName(serviceName).getName());
+    }
+
+    // todo: for some reason it fails
+    @Test
+    public void numberOfServices() {
+        assertEquals(count, serviceUnitService.numberOfServices());
+    }
+
+    @Test
+    public void updateService() {
+        ServiceUnit serviceUnit = createTestService();
+        String newName = "new name";
+        String description = "test description";
+
+        serviceUnit.setName(newName);
+        serviceUnit.setDescription(description);
+        serviceUnitService.save(serviceUnit);
+        assertEquals(description,
+                serviceUnitService.getByServiceName(newName).getDescription());
+    }
+}
