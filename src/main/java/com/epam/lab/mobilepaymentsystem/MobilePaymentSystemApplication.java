4e59c211b9480698ddd59004d76b951d478c2020
vlavik007 <vlavik007@gmail.com>
2018-07-28 21:00:14 +0300

    Small code style following

--- src/main/java/com/epam/lab/mobilepaymentsystem/MobilePaymentSystemApplication.java
+++ src/main/java/com/epam/lab/mobilepaymentsystem/MobilePaymentSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class MobilePaymentSystemApplication  extends WebMvcConfigurerAdapter{
+public class MobilePaymentSystemApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(MobilePaymentSystemApplication.class, args);
